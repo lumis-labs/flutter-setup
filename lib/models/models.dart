21f6f9bf6479886565b795f8aa1cc5aef376b060
zana-a <zana200@icloud.com>
2024-09-06 22:21:47 +0100

    remove dart

--- lib/models/models.dart
+++
@@ -1,8 +0,0 @@
-export 'channel.dart';
-export 'current_release.dart';
-export 'metadata.dart';
-export 'platform.dart';
-export 'release.dart';
-export 'architecture.dart';
-export 'environment.dart';
-export 'version.dart';
--- lib/services/services.dart
+++
@@ -1,1 +0,0 @@
-export 'metadata_service.dart';
